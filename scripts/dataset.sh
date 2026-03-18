4ec50bfa29a114a56c250cb36683e70cad5c121d
Wwhawhawinn <winnyscge@gmail.com>
2026-03-19 04:56:36 +0700

    Unused directory.

--- scripts/dataset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-NAME=fgnet-dataset
-curl -L -o ./${NAME}.zip \
-  https://www.kaggle.com/api/v1/datasets/download/aiolapo/fgnet-dataset
-unzip -q ./${NAME}.zip -d ./dataset
-rm -f ./${NAME}.zip
